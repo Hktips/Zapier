8a835c680eecbb98137b552e42f0df8a590b865a
hktips <hemant210433@gmail.com>
2025-10-03 13:00:32 +0530

    added signup

--- primary-backend/src/router/user.ts
+++ primary-backend/src/router/user.ts
@@ -39,49 +39,4 @@ router.post("/signup",async (req , res)=>{
 
 })
 
-router.post("/signin",async(req, res)=>{
-    const body=req.body.username;
-    const parsedData=SigninSchema.safeParse(body);
-    if(!parsedData.success){
-        return res.status(411).json({
-            message:"Incorrect inputs"
-        })
-    }
-    const user=await PrismaClient.user.findFirst({
-        where:{
-            email:parsedData.data.username,
-            pasword: parsedData.data.password
-        }
-    })
-    if(!user){
-        return res.status(403).json({
-            message:"Sorry credentials are incorrect"
-        });
-    }
-    const token = jwt.sign({
-        id:user.id
-
-    }, JWT_PASSWORD);
-    res.json({
-        token:token,
-    })
-})
-
-
-router.get("/user",authMiddleware,async(req,res)=>{
-    const id =req.id;
-    const user=await PrismaClient.user.findFirst({
-        where:{
-            id
-        },
-        select:{
-            name:true,
-            email:true
-        }
-    })
-    return res.json({
-        user
-    });
-})
-
 export const zapRouter=router;
